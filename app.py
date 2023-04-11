97557c1fc3e7725b06b8aa5f1f729b8f7306a0c6
Guillaume-4 <guillaume.blas4@gmail.com>
2023-04-11 10:26:55 +0200

    flask qui ""marche""

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
 @app.route('/morse')
 def autre_page():
     return render_template("/morse/morse.html")
