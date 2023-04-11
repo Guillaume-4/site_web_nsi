ebcc58e11d928ac80b778ae5e5ba1ec5e5cd0b07
Guillaume-4 <guillaume.blas4@gmail.com>
2023-04-11 09:42:55 +0200

    jsp gros

--- script/cesar.py
+++ script/cesar.py
@@ -1,12 +1,12 @@
 dico_alpha = {'a': 1,'b': 2,'c': 3,'d': 4,'e': 5,'f': 6,'g': 7,'h': 8,'i': 9,'j': 10,'k': 11,'l': 12,'m': 13,'n': 14, 'o': 15,'p': 16,'q':17,'r': 18,'s': 19,'t': 20,'u': 21,'v': 22,'w': 23,'x': 24,'y': 25,'z': 26}
 
-def coder(phrase):
+def coder(phrase, nb):
     phrase_terminer = ""
     phrase_code = []
     for i in range(len(phrase)):
         for ele in dico_alpha.items():
             if phrase[i] == ele[0]:
-                v = ele[1] + x
+                v = ele[1] + nb
         for ele in dico_alpha.items():
             if v == ele[1]:
                 phrase_code.append(ele[0])
@@ -20,5 +20,5 @@ def decoder(phrase_deco):
 
 x = int(input("chiffre"))
 phrase = 'ceci'
-print(coder(phrase))
+print(coder(phrase, x))
 
