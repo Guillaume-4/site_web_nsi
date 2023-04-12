77d73806139a53ec8e17d701161c0f6d44cbcd6e
Guillaume-4 <guillaume.blas4@gmail.com>
2023-04-12 14:51:36 +0200

    césar + morse qui marchent + page pigpen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from templates.script.morse import morse_encode, morse_decode
+from templates.script.cesar import cesar_coder, cesar_decoder
 
 app = Flask(__name__)
 
@@ -26,9 +27,27 @@ def autre_page():
     
 
 
-@app.route('/cesar')
+@app.route('/cesar', methods=['GET', 'POST']    )
 def cesar():
-    return render_template("/cesar/cesar.html")
+    phrase_a_coder = request.form.get("cesar_code")
+    phrase_a_decoder = request.form.get("cesar_decode")
+    if phrase_a_coder == None and phrase_a_decoder != None:
+        print(phrase_a_decoder)
+        decoder = cesar_decoder(phrase_a_decoder)
+        print(decoder)
+        return render_template("/cesar/cesar.html") + f"<h1>La Phrase décoder est: {decoder}</h1> "
+    elif phrase_a_coder != None and phrase_a_decoder == None:
+        print(phrase_a_coder)
+        coder = cesar_coder(phrase_a_coder)
+        print(coder)
+        return render_template("/cesar/cesar.html") + f"<h1>La Phrase coder est: {coder}</h1> "
+    else:
+        return render_template("/cesar/cesar.html")
+
+@app.route("/pigpen")
+def pigpen():
+    return render_template("pigpen/pigpen.html")
+
 
 phrase_a_decoder = None
 phrase_a_coder = None
